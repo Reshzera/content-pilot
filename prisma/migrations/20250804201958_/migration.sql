dc315f7f78e62c067a982381be24ae572d8c2649
Reshzera <reoshiro13@gmail.com>
2025-08-04 16:20:36 -0400

    feat: remove videos thumburl

---
+++ prisma/migrations/20250804201958_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `thumbnailUrl` on the `Videos` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Videos" DROP COLUMN "thumbnailUrl";
